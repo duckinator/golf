70bc320997cf906ef5f4d535bd5ffb4e8c03e683
Nick Markwell <nick@duckinator.net>
2012-06-13 01:41:24 -0400

    Moved nil-check

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,3 @@
-R=Hash[?I,1,?V,5,?X,10,?L,50,?C,100,?D,500,?M,1000];def r s,t=0
-s.gsub(/[IVXLCD]M|[IVXLC]D|[IVXL]C|[IVX]L|[IV]X|IV|[IVXLCDM]/){|m|o,n=R[m[0]]||0,R[m[1]]||0;o<n&&o=-o;t+=n+o}
+R=Hash[?I,1,?V,5,?X,10,?L,50,?C,100,?D,500,?M,1000,nil,0];def r s,t=0
+s.gsub(/[IVXLCD]M|[IVXLC]D|[IVXL]C|[IVX]L|[IV]X|IV|[IVXLCDM]/){|m|o,n=R[m[0]],R[m[1]];o<n&&o=-o;t+=n+o}
 t;end
